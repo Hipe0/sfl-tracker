e1f494c8e82257d20e219d88968cb1e5d64c6b98
Hồ Đắc Hiệp <hodachiep27@gmail.com>
2026-08-11 05:22:31 +0700

    done

--- clear_all.cjs
+++ clear_all.cjs
@@ -1,10 +1,9 @@
 const { MongoClient } = require('mongodb');
-require('dotenv').config({ path: '.env.local' });
 require('dotenv').config();
 const client = new MongoClient(process.env.MONGODB_URI);
 client.connect().then(async () => {
-  const db = client.db(process.env.MONGODB_DB_NAME);
-  const result = await db.collection('farm_history').updateMany({}, { $set: { bounties: [], animals: [] } });
-  console.log('Cleared bounties and animals for ' + result.modifiedCount + ' farms.');
+  const db = client.db('sfl_tracker');
+  const result = await db.collection('history').updateMany({}, { $set: { animals: [], animals_completed: {}, bounties: [], bounties_completed: {} } });
+  console.log('Cleared animals and bounties for ' + result.modifiedCount + ' farms.');
   client.close();
 });
